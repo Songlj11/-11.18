cbc915e377bbd6baf0e59ac9b2c30655700ea10c
李治江 <Lee@riharuetekiiMac.local>
2015-07-21 09:32:09 +0800

    Initial Commit

---
+++ GuJinBao/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GuJinBao
+//
+//  Created by 李治江 on 15/7/21.
+//  Copyright (c) 2015年 GuJinBao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GuJinBao/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GuJinBao
+//
+//  Created by 李治江 on 15/7/21.
+//  Copyright (c) 2015年 GuJinBao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
